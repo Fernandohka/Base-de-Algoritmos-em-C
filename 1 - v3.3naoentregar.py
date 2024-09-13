4724fd00bd47dd0047c995dbedce78f8562403ca
Fernandohka <fernando2005andrade@gmail.com>
2024-09-13 14:29:29 -0300

    ex 1 e 2

---
+++ 1 - v3.3naoentregar.py
@@ -0,0 +1,22 @@
+#https://github.com/Andredev-dias/Base-de-algoritmos-em-C-v3/blob/master/3naoentregar.c
+
+def Calcula(p1, p2):
+    Aux = 0
+    Aux = (int)(p1) + ((int)(p1) * ((float)(10/100)))
+    p2 = (int)(p1) + ((int)(p1) * ((float)(20/100)))
+    p1 = (int)(p1) + ((int)(p1) * ((float)(15/100)))
+    return Aux
+
+def Mostra(p_Sal_10, p_Sal_15, p_Sal_20):
+    print(f"O salario 1 é {p_Sal_10}")
+    print(f"O salario 2 é {p_Sal_15}")
+    print(f"O salario 3 é {p_Sal_20}")
+
+Sal_10 = 0
+Sal_15 = 0
+Sal_20 = 0
+
+Sal_15 = input("informe: ")
+
+Calcula(Sal_15, Sal_20)
+Mostra(Sal_10, Sal_15, Sal_20)
---
+++ exercicio 2.py
@@ -0,0 +1,6 @@
+# https://github.com/Andredev-dias/Base-de-Algoritmos-em-C/blob/master/abs.c
+
+x = int(input("Qual o primeiro numero: "))
+y = int(input("Qual o segundo numero: "))
+resp = abs(x-y)
+print(f"A diferença entre {x} e {y} é: {resp}")
